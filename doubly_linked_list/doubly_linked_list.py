a0b53982ff8d8f40a43ae10e1788a029f302075b
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-09-09 18:06:27 -0400

    MVP for day 3

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -40,23 +40,10 @@ class DoublyLinkedList:
     """
 
     def add_to_head(self, value):
-        #   Create instance of ListNode w/ Value
-        #   Increment the DLL length attribute
-
-        #   Check if DLL is empty
-        #   if it is empty
-        #   set head and tail to the new node instance
-
-        #   If DLL is not empty
-        #   set new node's next to current head
-        #   set head's prev to new node
-        #   set head to the new node
         current_head = self.head
-        #   If head exists
         if self.head:
             self.head = ListNode(value, None, current_head)
             current_head.prev = self.head
-        #   If head does not exist then tail does not exist. Must assign new node to head AND tail.
         else:
             self.head = ListNode(value)
             self.tail = self.head
@@ -69,19 +56,8 @@ class DoublyLinkedList:
     """
 
     def remove_from_head(self):
-        #   Store the value of the head
-        #   Decrement the head
-        #   Delete the head
-        #   if head.next is not None
-        #   set head.next's prev to None
-        #   set hed to head.next
-
-        #   Return the value
-
-        #   If head exists
         if self.head:
             current_head = self.head
-            #   If there is only one node both head and tail should be None
             if self.head == self.tail:
                 self.head = None
                 self.tail = None
@@ -100,23 +76,11 @@ class DoublyLinkedList:
     """
 
     def add_to_tail(self, value):
-        #   Create instance of ListNode with value
-        #   Increment the DLL length attribute
 
-        #   If DLL is empty
-        #   set head and tail to the new node instance
-
-        #   If DLL is not empty
-        #   set new node's prev to current tail
-        #   set tail's next to new node
-        #   set tail to the new node
-
-        #   If tail exists
         if self.tail:
             newNode = ListNode(value, self.tail)
             self.tail.next = newNode
             self.tail = newNode
-            #   Tail does not exist then head does not exist
         else:
             self.tail = ListNode(value)
             self.head = self.tail
@@ -129,25 +93,10 @@ class DoublyLinkedList:
     """
 
     def remove_from_tail(self):
-
-        #   Store the value of the tail
-        #   Decrement the length of the DLL
-        #   Delete the tail
-        #   if tail.prev is not None
-        #   set tail.prev's next to None
-        #   set tail to tail.prev
-        #   els (if tail.prev is None)
-        #   set head to None
-        #   set tail to None
-
-        #   Return the value
-
-        #   If tail exists
         current_tail = self.tail
         if self.tail:
             self.length -= 1
             if self.tail == self.head:
-                #   --or--  self.length ==1
                 self.tail = None
                 self.head = None
             else:
@@ -164,7 +113,6 @@ class DoublyLinkedList:
     """
 
     def move_to_front(self, node):
-        #   Always check if exists
         if self.head:
             node.delete()
             current_head = self.head
@@ -178,7 +126,6 @@ class DoublyLinkedList:
     """
 
     def move_to_end(self, node):
-        #   Check if tail exists
         if self.tail:
             self.remove_from_head()
             current_tail = self.tail
@@ -187,7 +134,6 @@ class DoublyLinkedList:
             self.tail.prev = current_tail
             self.length += 1
         else:
-
             current_tail = self.tail
             node.delete()
             self.tail = node
